680984e16fbf37656f04ad22664bf41d62aa040b
Ryan Chase <ryan@chasery.com>
2019-08-14 09:19:17 -0700

    Add in basic responsive support to header

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,14 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+import { ReactComponent as MenuIcon } from "./svg/menu.svg";
 import { ReactComponent as EmailIcon } from "./svg/email.svg";
 import { ReactComponent as GithubIcon } from "./svg/github.svg";
 import { ReactComponent as LinkedInIcon } from "./svg/linkedin.svg";
 
-const NavWrapper = styled.nav`
+const NavWrapper = styled.div`
     display: flex;
     flex-grow: 1;
     justify-content: flex-end;
+    position: relative;
+    z-index: 20;
+`;
+const NavContent = styled.nav`
+    @media (max-width: ${props => props.theme.breakSm}) {
+        background-color: ${props => props.theme.white}
+        height: ${props => (props.navIsOpen ? "100vh" : "0")};
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: ${props => (props.navIsOpen ? "100vw" : "0")};
+        visibility: ${props => (props.navIsOpen ? "visible" : "hidden")};
+    }
 `;
 const NavControls = styled.div`
     align-items: center;
@@ -31,17 +45,9 @@ const NavLink = styled.button`
     padding: 0 1.25rem 0.25rem;
     text-transform: lowercase;
 
-    svg g {
-        fill: ${props => props.theme.mineShaft};
-    }
-
     &:hover {
         border-top: 0.25rem solid ${props => props.theme.turquoise};
         color: ${props => props.theme.turquoise};
-
-        svg g {
-            fill: ${props => props.theme.turquoise};
-        }
     }
 `;
 const IconLink = styled(NavLink)`
@@ -52,40 +58,61 @@ const IconLink = styled(NavLink)`
 
     svg {
         align-self: center;
+
+        g {
+            fill: ${props => props.theme.mineShaft};
+        }
     }
 
     &:hover {
         border-top: none;
+
+        g {
+            fill: ${props => props.theme.turquoise};
+        }
+    }
+`;
+const MenuButton = styled(IconLink)`
+    display: none;
+
+    @media (max-width: ${props => props.theme.breakSm}) {
+        display: block;
     }
 `;
 
-const Nav = () => {
+const Nav = props => {
+    console.log(props.navIsOpen);
     return (
         <NavWrapper>
-            <NavControls>
-                <NavLink>About</NavLink>
-                <NavLink>Skills</NavLink>
-                <NavLink>Portfolio</NavLink>
-            </NavControls>
-            <NavControls>
-                <IconLink
-                    as="a"
-                    href="https://github.com/chasery/"
-                    target="_blank"
-                >
-                    <GithubIcon />
-                </IconLink>
-                <IconLink
-                    as="a"
-                    href="https://www.linkedin.com/in/chasery"
-                    target="_blank"
-                >
-                    <LinkedInIcon />
-                </IconLink>
-                <IconLink as="a" href="mailto:test@test.com">
-                    <EmailIcon />
-                </IconLink>
-            </NavControls>
+            <MenuButton onClick={props.toggleMobileNav}>
+                <MenuIcon />
+            </MenuButton>
+            <NavContent navIsOpen={props.navIsOpen}>
+                <NavControls>
+                    <NavLink>About</NavLink>
+                    <NavLink>Skills</NavLink>
+                    <NavLink>Portfolio</NavLink>
+                </NavControls>
+                <NavControls>
+                    <IconLink
+                        as="a"
+                        href="https://github.com/chasery/"
+                        target="_blank"
+                    >
+                        <GithubIcon />
+                    </IconLink>
+                    <IconLink
+                        as="a"
+                        href="https://www.linkedin.com/in/chasery"
+                        target="_blank"
+                    >
+                        <LinkedInIcon />
+                    </IconLink>
+                    <IconLink as="a" href="mailto:test@test.com">
+                        <EmailIcon />
+                    </IconLink>
+                </NavControls>
+            </NavContent>
         </NavWrapper>
     );
 };
---
+++ src/components/header/Header.js
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+
+import Nav from "./Nav";
+import { ReactComponent as Logo } from "./svg/ryan-chase-logo.svg";
+
+const HeaderWrapper = styled.header`
+    background-color: ${props => props.theme.white};
+    box-shadow: 0 0.0625rem 0.0625rem rgba(0, 0, 0, 0.1);
+    display: flex;
+    left: 0;
+    justify-content: center;
+    position: fixed;
+    width: 100vw;
+    top: 0;
+    z-index: 10;
+`;
+const HeaderContent = styled.div`
+    display: flex;
+    padding: 0 0.625rem 0 1.25rem;
+
+    @media (max-width: ${props => props.theme.breakSm}) {
+        width: ${props => props.theme.contentSm};
+    }
+    @media (min-width: ${props =>
+            props.theme.breakSm}) and (max-width: ${props =>
+            props.theme.breakLrg}) {
+        width: ${props => props.theme.contentMed};
+    }
+    @media (min-width: ${props => props.theme.breakLrg}) {
+        width: ${props => props.theme.contentLrg};
+    }
+`;
+const HeaderLogo = styled.a`
+    align-items: center;
+    display: flex;
+    color: ${props => props.theme.mineShaft};
+    font-size: 1.25rem;
+    font-weight: 700;
+    padding: ${props => props.theme.spacingSm} 0;
+    text-decoration: none;
+    text-transform: lowercase;
+
+    & > svg {
+        margin-right: ${props => props.theme.spacingMed};
+    }
+`;
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { navIsOpen: false };
+    }
+
+    toggleMobileNav = () => {
+        this.setState(prevState => ({
+            navIsOpen: !prevState.navIsOpen
+        }));
+    };
+
+    render() {
+        return (
+            <HeaderWrapper>
+                <HeaderContent>
+                    <HeaderLogo href="/">
+                        <Logo />
+                        <div>Ryan Chase</div>
+                    </HeaderLogo>
+                    <Nav
+                        navIsOpen={this.state.navIsOpen}
+                        toggleMobileNav={this.toggleMobileNav}
+                    />
+                </HeaderContent>
+            </HeaderWrapper>
+        );
+    }
+}
+
+export default Header;
