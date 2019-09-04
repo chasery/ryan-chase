8d64abc5319e560473d1d51908f9e3e24ef2e5c3
Ryan Chase <ryan@chasery.com>
2019-09-04 07:06:31 -0700

    Initializing component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+import Section from "./shared/Section";
+
 const SkillsWrapper = styled.section`
     height: 100vh;
 `;
 
 const Skills = props => {
-    return <SkillsWrapper id={props.id}>Skills</SkillsWrapper>;
+    return <Section id={props.id}>Skills</Section>;
 };
 
 export default Skills;
