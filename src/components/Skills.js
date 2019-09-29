d4c367b006c6efbf0b9a8c874b9287e66fc0bd56
Ryan Chase <ryan@chasery.com>
2019-09-29 12:18:40 -0700

    Add initial component structure

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import Section from "./shared/Section";
 
-const SkillsWrapper = styled.section`
-    height: 100vh;
+const SkillsContent = styled.div`
+    display: flex;
+
+    @media (max-width: ${props => props.theme.breakSm}) {
+        flex-direction: column;
+    }
 `;
+const TabsWrapper = styled.div``;
+const TabsControlWrapper = styled.div``;
+const TabControl = styled.button``;
+const TabsContentWrapper = styled.div``;
+const Tab = styled.div``;
 
 const Skills = props => {
-    return <Section id={props.id}>Skills</Section>;
+    return (
+        <Section id={props.id}>
+            <SkillsContent>
+                <TabsWrapper>
+                    <TabsControlWrapper>
+                        <TabControl>Front-End Development</TabControl>
+                        <TabControl>User Experience</TabControl>
+                    </TabsControlWrapper>
+                    <TabsContentWrapper>
+                        <Tab>
+                            <p>
+                                Ryan currently works as a senior UX Designer,
+                                where he leads a team that drives an empathic
+                                approach to software. His background in design
+                                and development fosters a collaborative
+                                environment between the two groups, resulting in
+                                the creation of the best product possible.
+                            </p>
+                        </Tab>
+                        <Tab>
+                            <p>
+                                Ryan currently works as a senior UX Designer,
+                                where he leads a team that drives an empathic
+                                approach to software. His background in design
+                                and development fosters a collaborative
+                                environment between the two groups, resulting in
+                                the creation of the best product possible.
+                            </p>
+                        </Tab>
+                    </TabsContentWrapper>
+                </TabsWrapper>
+            </SkillsContent>
+        </Section>
+    );
 };
 
 export default Skills;
